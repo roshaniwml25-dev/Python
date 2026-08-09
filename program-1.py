0e07aeb184b881b138d73cef4f5f514766a6b561
roshaniwml25-dev <roshaniw.ml25@sbjit.edu.in>
2026-08-09 16:13:59 +0530

    Add files via upload

---
+++ program-1.py
@@ -0,0 +1,11 @@
+
+numbers = [10, 20, 30, 40, 50]
+
+print("Third element:", numbers[2])
+
+print("List length:", len(numbers))
+
+if len(numbers) == 0:
+    print("The list is empty.")
+else:
+    print("The list is not empty.")
