b3c851f8021b0af891c3dc3f4471590b513b77af
roshaniwml25-dev <roshaniw.ml25@sbjit.edu.in>
2026-08-13 22:14:17 +0530

    add a new key-value pair to a dictionary,

---
+++ program-11.py
@@ -0,0 +1,9 @@
+student = {"name": "Alice", "age": 20, "grade": "B"}
+
+student["city"] = "Mumbai"
+
+student["grade"] = "A"
+
+print("Student name:", student["name"])
+
+print("Updated dictionary:", student)
