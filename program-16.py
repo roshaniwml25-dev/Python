1d5c46061e39f47345aa4b5cba93a658b3ff1ced
roshaniwml25-dev <roshaniw.ml25@sbjit.edu.in>
2026-08-17 21:27:29 +0530

    create tupple

---
+++ program-16.py
@@ -0,0 +1,8 @@
+fruits = ("apple", "banana", "mango", "orange")
+
+print("First element:", fruits[0])
+print("Second element:", fruits[1])
+print("Third element:", fruits[2])
+print("Fourth element:", fruits[3])
+
+print("Length of tuple:", len(fruits))
